d2678f62e5573596d4326fb6a3f7d25894c4ff52
prasouk-ph <prasouk.ph@gmail.com>
2022-02-23 13:58:28 +0100

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App/>}>
+                </Route>
+                {/* <Route path="/about" element={<About/>}>
+                </Route>
+                <Route path=“*” element={<NoPage/>}> */}
+                {/* </Route> */}
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
