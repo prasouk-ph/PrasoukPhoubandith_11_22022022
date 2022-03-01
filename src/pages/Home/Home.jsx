f5fee7f7cd9d2923f5d4884b3c40f7bf9d511113
prasouk-ph <prasouk.ph@gmail.com>
2022-03-01 15:44:33 +0100

    Refactoring

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,16 +3,14 @@ import Card from './Card/Card';
 import accomodations from '../../data/logements.json'
 
 function Home() {
-    const accomodationsData = accomodations
-
-    return (
+return (
     <main>
         <section className="home-hero">
             <p className='hero-text'>Chez vous, partout et ailleurs</p>
         </section>
         
         <section className='card-container'>
-                {accomodationsData.map(accomodation => (
+                {accomodations.map(accomodation => (
                     <Card title={accomodation.title} key={accomodation.id} id={accomodation.id} />
                 ))}
         </section>
